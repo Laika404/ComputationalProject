5ef794c3b74d10dda553e9daf7ab5671d349710c
HamidA7777 <hamid.ahmadi@student.uva.nl>
2025-01-16 15:38:27 +0100

    Finalized simulation code & added images

--- model.py
+++ model.py
@@ -10,12 +10,13 @@ class Model(object):
         self.total_time = total_time
         self.road_length = road_length
         self.road_length_km = self.road_length / 1000
-        self.density_values = np.linspace(0, 140, 10)
-        self.flow_results = []
-        self.speed_results = []
+        self.density_values = np.linspace(0, 120, 10)
+        self.total_runs = 20 # In the paper they also did 20 runs for each density
+        self.flow_results = [[] for _ in range(self.total_runs)]
+        self.speed_results = [[] for _ in range(self.total_runs)]
 
 
-    def run(self) -> None:
+    def run(self, idx) -> None:
         for density in self.density_values:
             # N is amount of vehicles
             N = int(density * self.road_length_km)
@@ -23,7 +24,7 @@ class Model(object):
             initial_positions += np.arange(N) * 5  # Ensure minimum gaps of 5m by adding vehicle length
             
             vehicles: List[VehicleAgent] = [
-                VehicleAgent(pos, 30) for pos in initial_positions
+                VehicleAgent(pos, np.random.uniform(0,35)) for pos in initial_positions
             ]
             
             total_crossings = 0
@@ -47,23 +48,43 @@ class Model(object):
                         vehicle.position -= self.road_length
                         total_crossings += 1
 
-            flow = (total_crossings / self.total_time) * 3600
+            flow = total_crossings
             mean_speed = np.mean([vehicle.current_speed for vehicle in vehicles])
             
-            self.flow_results.append(flow)
-            self.speed_results.append(mean_speed)
+            self.flow_results[idx].append(flow)
+            self.speed_results[idx].append(mean_speed)
             
             
     def plot(self, stat: str = "position", out_file=None) -> None:
         if stat == "position":
-            plt.plot(self.density_values, self.flow_results)
-            plt.xlabel('density (veh/km)')
-            plt.ylabel('flow (veh/h)')
+            for idx in range(self.total_runs): # Run the simulation 20 times
+                self.run(idx)
+            
+                # Scatter plot for current run
+                plt.scatter(self.density_values, self.flow_results[idx], alpha=0.5, color='gray')
+        
+            # average flow
+            avg_flow = np.mean(self.flow_results, axis=0) if isinstance(self.flow_results[0], list) else self.flow_results
+            plt.plot(self.density_values, avg_flow, color='black', label='Mean Flow')
+            plt.xlabel('Density (veh/km)')
+            plt.ylabel('Flow (veh/h)')
+            plt.title('The relationship between flow and density')
+            plt.legend()
 
         elif stat == "velocity":
-            plt.plot(self.density_values, self.speed_results)
-            plt.xlabel('density (veh/km)')
-            plt.ylabel('mean speed (m/s)')
+            for idx in range(20): # Run the simulation 20 times
+                self.run(idx)
+            
+                # Scatter plot for current run
+                plt.scatter(self.density_values, self.speed_results[idx], alpha=0.5, color='gray')
+        
+            # average speed
+            avg_speed = np.mean(self.speed_results, axis=0) if isinstance(self.speed_results[0], list) else self.speed_results
+            plt.plot(self.density_values, avg_speed, color='black', label='Mean Speed')
+            plt.xlabel('Density (veh/km)')
+            plt.ylabel('Mean Speed (m/s)')
+            plt.title('The relationship between mean-speed and density')
+            plt.legend()
 
         else:
             raise ValueError(f"Unrecognised statistic '{stat}'")
@@ -74,6 +95,5 @@ class Model(object):
             plt.savefig(out_file)
 
 
-m = Model()
-m.run()
-m.plot(stat="velocity")
+model = Model()
+model.plot(stat="velocity")
