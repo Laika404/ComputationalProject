5c9bac0a689db47516d06198b5d176b7f05513d0
Sietse <94106259+Laika404@users.noreply.github.com>
2025-01-21 11:49:59 +0100

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -3,7 +3,16 @@ import matplotlib.pyplot as plt
 
 # car object that behaves like a car
 class VehicleAgent(object):
-    def __init__(self, position, current_speed, desired_speed=30, max_speed=35, length=5, a_normal=3.05, a_max=6.04, b=0.2, TP=1.2):
+
+    dt = None
+
+    # dictionary used for optimization
+    enum_param = {"max_speed": 0, "desired_speed": 1, "TP": 2,
+                  "a_normal": 3, "a_max": 4, "b":5}
+    
+    def __init__(self, track=None, position=None, current_speed=None, desired_speed=30, max_speed=35, length=5, a_normal=3.05, a_max=6.04, b=0.2, TP=1.2):
+        # is of class Lane
+        self.track = track
         self.position = position
         self.current_speed = current_speed
         self.desired_speed = desired_speed
@@ -14,6 +23,32 @@ class VehicleAgent(object):
         self.b = b
         self.TP = TP
         self.acceleration = 0
+
+        # used for parallel updates
+        self.next_speed = None
+        self.next_position = None
+
+        self.pos_data = [0]
+        self.speed_data = [0]
+
+
+    def change_parameter(self, param: str, param_val):
+        param_number = self.__class__.enum_param[param]
+        # change certain parameter
+        match param_number:
+            case 0:
+                self.max_speed = param_val
+            case 1:
+                self.desired_speed = param_val
+            case 2:
+                self.TP = param_val
+            case 3:
+                self.a_normal = param_val
+            case 4:
+                self.a_max = param_val
+            case 5:
+                self.b = param_val
+
 
     def compute_decision(self, gap, leader_speed, leader_acceleration):
         """
@@ -122,10 +157,22 @@ class VehicleAgent(object):
         v_safe = leader_speed + ((gap - leader_speed*reaction_time) / (reaction_time + ((self.current_speed + leader_speed) / (2 * self.a_max))))
         return v_safe
 
-    def update_state(self, gap, leader_speed, leader_acceleration, dt):
+    def update_next(self, leader):
+        leader_speed = leader.current_speed
+        leader_acceleration = leader.acceleration
+        gap = self.track.get_gap(leader, self)
+
         self.compute_decision(gap, leader_speed, leader_acceleration)
         v_safe = self.compute_safe_speed(gap, leader_speed)
-        v_ideal = min(self.max_speed, self.current_speed + self.acceleration * dt, v_safe)
+        v_ideal = min(self.max_speed, self.current_speed + self.acceleration * self.__class__.dt, v_safe)
         eta = np.random.uniform(0,1)
-        self.current_speed = max(0, v_ideal - self.b * eta)
-        self.position += self.current_speed * dt
+        
+        self.next_speed = max(0, v_ideal - self.b * eta)
+        self.next_position = self.position + self.current_speed * self.__class__.dt
+    
+    def update_state(self):
+        self.current_speed = self.next_speed
+        self.position = self.next_position
+
+        self.pos_data.append(self.position)
+        self.speed_data.append(self.current_speed)
