7839a4e4f9ab6699f8b71d7a775d495d481c4079
HamidA7777 <hamid.ahmadi@student.uva.nl>
2025-01-21 12:41:28 +0100

    Added comments for model.py

--- model.py
+++ model.py
@@ -6,6 +6,13 @@ from Agent import VehicleAgent
 
 class Model(object):
     def __init__(self, dt: float = 1.0, total_time: int = 2000, road_length: int = 2000) -> None:
+        """
+        The parameters of the simulation model are:
+        - dt: the timestep in each iteration of the simulation
+        - total_time: the total duration of the simulation
+        - road_length: the length of the single lane
+        """
+
         self.dt = dt
         self.total_time = total_time
         self.road_length = road_length
@@ -17,6 +24,11 @@ class Model(object):
 
 
     def run(self, idx) -> None:
+        """
+        A single run of the simulation. In total, we will perform 20 runs, 
+        which is also done in the paper which we wanted to validate our model with.
+        """
+
         for density in self.density_values:
             # N is amount of vehicles
             N = int(density * self.road_length_km)
@@ -24,10 +36,10 @@ class Model(object):
             initial_positions += np.arange(N) * 5  # Ensure minimum gaps of 5m by adding vehicle length
             
             vehicles: List[VehicleAgent] = [
-                VehicleAgent(pos, np.random.uniform(0,35)) for pos in initial_positions
+                VehicleAgent(pos, np.random.uniform(0,35)) for pos in initial_positions # speed is initialied at random, must be between 0 and max speed = 35
             ]
             
-            total_crossings = 0
+            total_crossings = 0 # count the total crossings at a fixed reference point in time
 
             for t in range(int(self.total_time / self.dt)):
                 vehicles.sort(key = lambda vehicle: vehicle.position)
@@ -37,6 +49,7 @@ class Model(object):
                     gap = (leader.position - vehicle.position) % self.road_length
                     gap = max(0, gap - leader.length)
 
+                    # update the state of the current vehicle(the follower)
                     vehicle.update_state(
                         gap=gap,
                         leader_speed=leader.current_speed,
@@ -44,6 +57,8 @@ class Model(object):
                         dt=self.dt,
                     )
 
+                    # if the vehicle's traveled distance is > 2000m then we wrap around the road
+                    # which is called the periodic boundary condition.
                     if vehicle.position >= self.road_length:
                         vehicle.position -= self.road_length
                         total_crossings += 1
@@ -56,6 +71,12 @@ class Model(object):
             
             
     def plot(self, stat: str = "position", out_file=None) -> None:
+        """
+        Make the plot, which is either a flow-density graph or mean-speed-density graph. 
+        If stat = position, then the plot is a flow-density graph. If stat = velocity
+        then the plot is a mean-speed-density graph.
+        """
+
         if stat == "position":
             for idx in range(self.total_runs): # Run the simulation 20 times
                 self.run(idx)
@@ -96,4 +117,4 @@ class Model(object):
 
 
 model = Model()
-model.plot(stat="velocity")
+model.plot(stat="position")
